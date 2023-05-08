e6f113ac4d84b445ca06e913285a5f73c2469000
Nastya <P19042009@yandex.ru>
2023-05-08 16:47:34 +0300

    check to spam

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,18 @@
 function checkSpam(str) {
-  // ваш код...
+  let newStr = str.toLowerCase(),
+      foundStavki = newStr.indexOf('1xbet'),
+      result = isFound(foundStavki);
+  if (!(result)) {
+      foundStavki = newStr.indexOf('xxx');
+      result = isFound(foundStavki)
+  }
+return result
+}
+
+function isFound (index) {
+  if (index == -1) {
+      return false
+  } else {
+      return true
+  }
 }
