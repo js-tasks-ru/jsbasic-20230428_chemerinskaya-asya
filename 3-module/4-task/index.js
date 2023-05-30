23bcb577a872b205dfc408f23a4a8f443e72e89a
Nastya <P19042009@yandex.ru>
2023-05-30 19:00:07 +0300

    fn showSalary

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
-  // ваш код...
+  let ageFilterUsers = users.filter(item => item.age <= age);
+  let newStr = '';
+  for (let i = 0; i < (ageFilterUsers.length - 1); i++) {
+      newStr += ageFilterUsers[i].name + ', ' + ageFilterUsers[i].balance + '\n'
+  }
+  newStr += ageFilterUsers[ageFilterUsers.length - 1].name + ', ' + ageFilterUsers[ageFilterUsers.length - 1]
+      .balance
+  return newStr
 }
